fa60eb5c7f0113b240f2844f1b149ba36cf72d67
norman chang <nsc.chang@gmail.com>
2015-06-22 20:04:50 +0800

    finish CRUD for forums

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass', '~>3.3.4'
 
-gem 'kaminari'
+#gem 'kaminari'
 
 #gem 'devise'
 
--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,40 +1,60 @@
 class ForumsController < ApplicationController
-  before_action :find_post
+  before_action :find_post, :only=>[:update, :destroy]
 
   def index
-    @forum=Forum.all
-  end
 
-  def create
+    @forums=Forum.all
+
     @forum=Forum.new
   end
 
+  def create
+    @forum=Forum.new(forum_params)
+    if @forum.save
+      flash[:notice]="Create Succeeded"
+      redirect_to forums_path
+    else
+      @forums=Forum.all
+      render :action=>:index
+    end
+  end
+
   def update
+    @forum=Forum.update(params[:id],forum_params)
+    if @forum.save
+      flash[:notice]="Update Succeeded"
+      redirect_to forums_path
+    else
+      render :action=>:edit
+    end
+
   end
 
   def destroy
     @forum.destroy
-    flash[:alert] = "刪除成功"
+    flash[:alert] = "Delete Succeeded"
+    redirect_to forums_path
   end
 
   def edit
-  end
-
-  def new
+    @forum=Forum.find(params[:id])
   end
 
   def show
+    @forum=Forum.find(params[:id])
   end
 
 
   protected
 
   def find_post
-    @forum=Forum.find(params[:postid])
+    @forum=Forum.find(params[:id])
   end
 
-  def location_params
-    params.require(:location).permit(:name)
+  def forum_params
+
+    params.require(:forum).permit(:topic, :body)
+
   end
 
 end
